88a7b5b8affb028cb93e9a6ec57c552273ee6c6d
Sushant Bhasin <sbhasin@Sushants-MacBook-Pro.local>
2020-11-03 17:38:09 +0530

    Updates

---
+++ precache-manifest.c5ce158cba1694a3c78c319fd4ef3af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29de36b6ec7e72c07122bad399e2a849",
+    "url": "/spa-sample-app/index.html"
+  },
+  {
+    "revision": "fde4394fef78b8f407ba",
+    "url": "/spa-sample-app/static/css/main.cf07d08c.chunk.css"
+  },
+  {
+    "revision": "74a456cefa27f7117508",
+    "url": "/spa-sample-app/static/js/2.1827744e.chunk.js"
+  },
+  {
+    "revision": "21c75a98ff1b0cc08c27fd17e11b8c68",
+    "url": "/spa-sample-app/static/js/2.1827744e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde4394fef78b8f407ba",
+    "url": "/spa-sample-app/static/js/main.982bce02.chunk.js"
+  },
+  {
+    "revision": "a59c45281576dd2b5952",
+    "url": "/spa-sample-app/static/js/runtime-main.3ecbce96.js"
+  }
+]);
